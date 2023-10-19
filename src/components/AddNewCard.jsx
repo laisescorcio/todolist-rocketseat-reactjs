e38c1148ec6138f3dded3aa25982ed86b001000c
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2023-10-18 21:05:41 -0300

    feat: use useState and useRef to get input value

--- src/components/AddNewCard.jsx
+++ src/components/AddNewCard.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import insertIcon from '../assets/insertIcon.svg'
 
 import styles from './AddNewCard.module.css'
 
 export default function AddNewCard({childToParent}) {
+    const inputRef = useRef(null)
+    const [ useToDoItem, setUseToDoItem ] = useState('')
     const data = "Estes são os dados do componente filho para o componente pai."
 
     function handleCreateNewCard(event) {
         event.preventDefault() //previne o default de que no clique o usuário seja redirecionado
-    
-        console.log('clique')
+        const inputValueRef = inputRef.current.value
+        console.log('inputValueRef', inputValueRef)
     }
+
+    useEffect(() => {
+        console.log(useToDoItem)
+    }, [useToDoItem])
 
     return (
         <form
@@ -18,8 +24,8 @@ export default function AddNewCard({childToParent}) {
         >
             <button primary onClick={() => childToParent(data)}>Clique no filho</button>
         <div className={styles.taskInputBox}>
-            <input className={styles.taskInput}/>
-            <button className={styles.taskButton}>
+            <input className={styles.taskInput} ref={inputRef} onChange={() => setUseToDoItem(event.target.value)}/>
+            <button className={styles.taskButton} >
               Criar
               <img src={insertIcon} />
             </button>
