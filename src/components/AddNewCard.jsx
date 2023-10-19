cd9c8e913e0d01f3192070af7181726ae60a5936
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2023-10-18 21:43:04 -0300

    Feat: updated the text card with the input content

--- src/components/AddNewCard.jsx
+++ src/components/AddNewCard.jsx
@@ -5,27 +5,34 @@ import styles from './AddNewCard.module.css'
 
 export default function AddNewCard({childToParent}) {
     const inputRef = useRef(null)
-    const [ useToDoItem, setUseToDoItem ] = useState('')
-    const data = "Estes são os dados do componente filho para o componente pai."
+    const [ toDoItem, setToDoItem ] = useState('')
+    const [ newToDoItem, setNewToDoItem ] = useState('')
+    const data = newToDoItem
 
     function handleCreateNewCard(event) {
         event.preventDefault() //previne o default de que no clique o usuário seja redirecionado
-        const inputValueRef = inputRef.current.value
-        console.log('inputValueRef', inputValueRef)
+
+        setToDoItem(...toDoItem, newToDoItem)
+        setNewToDoItem('')
+    }
+
+    function handleNewToDoItemChange() {
+        setNewToDoItem(event.target.value)
+        console.log('newToDoItem', newToDoItem)
     }
 
     useEffect(() => {
-        console.log(useToDoItem)
-    }, [useToDoItem])
+        handleNewToDoItemChange()
+    }, [newToDoItem])
 
     return (
         <form
             onSubmit={handleCreateNewCard}
         >
-            <button primary onClick={() => childToParent(data)}>Clique no filho</button>
+            <button primary >Clique no filho</button>
         <div className={styles.taskInputBox}>
-            <input className={styles.taskInput} ref={inputRef} onChange={() => setUseToDoItem(event.target.value)}/>
-            <button className={styles.taskButton} >
+            <input className={styles.taskInput} ref={inputRef} onChange={handleNewToDoItemChange}/>
+            <button className={styles.taskButton} onClick={() => childToParent(data)}>
               Criar
               <img src={insertIcon} />
             </button>
